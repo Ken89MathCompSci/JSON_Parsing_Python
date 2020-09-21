45866a0c066a43b067a126e6be9292687b0c689d
Ken89MathCompSci <lamerkenneth@hotmail.com>
2020-09-21 00:16:44 -0700

    Create Convert_JSON_To_Python.py

---
+++ Convert_JSON_To_Python.py
@@ -0,0 +1,13 @@
+# Python program to convert JSON to Python 
+  
+  
+import json 
+  
+# JSON string 
+employee ='{"id":"09", "name": "Nitin", "department":"Finance"}'
+  
+# Convert string to Python dict 
+employee_dict = json.loads(employee) 
+print(employee_dict) 
+  
+print(employee_dict['name']) 
